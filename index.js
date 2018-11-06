35fed218284c1506548e12bb264573d617dca541
Olya Bablinyuk <olyabablinyuk@Olyas-MacBook-Pro.local>
2018-11-07 08:41:40 +1300

    updated Hello World

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var time = require('time');
 exports.handler = (event, context, callback) => {
     callback(null, {
         statusCode: '200',
-        body: 'Hello World3!',
+        body: 'Hello World4!',
     });
 };
 
